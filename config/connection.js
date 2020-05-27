89e14a3be3602c1ed9320284561ab3ca2cb314fa
Brandon P <brandonj.peart1@gmail.com>
2020-05-27 18:53:50 -0400

    added a procfile.

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.DB_Password,
+  password: "@Jordan2814",
   database: "burger_db"
 });
 
